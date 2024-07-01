8ea6ec18c9fed33b430694a91b8ce503af1b91f9
Sopheanan <khemsopheanan.biu@gmail.com>
2024-07-01 23:28:32 +0700

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,32 @@
+function Yellow(){
+    document.getElementById("cl").style.color ="yellow";
+    document.getElementById("cl").innerHTML ="Yellow";
+}
+function Green(){
+    document.getElementById("cl").style.color = "green";
+    document.getElementById("cl").innerHTML ="Green";
+}
+function Red(){
+    document.getElementById("cl").style.color = "red";
+    document.getElementById("cl").innerHTML ="Red";
+}
+function Purple(){
+    document.getElementById("cl").style.color = "purple";
+    document.getElementById("cl").innerHTML ="Purple";
+}
+function Pink(){
+    document.getElementById("cl").style.color = "pink";
+    document.getElementById("cl").innerHTML ="Pink";
+}
+function Gray(){
+
+    document.body.style.background ="gray";
+}
+function White(){
+    document.body.style.background ="white";
+    document.querySelectorAll('h1')[0].style.color="black";
+}
+function Black(){
+    document.body.style.background ="black";
+    document.querySelectorAll('h1')[0].style.color="white";
+}
